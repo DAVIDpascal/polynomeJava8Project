4fc615b86b3705c48c3aa8714bcadd342057080a
Pascal david <purewebconsulting@gmail.com>
2020-03-18 18:22:09 +0100

    Implements polynomial project

---
+++ src/com/horner/polynome/HornerPolynomial.java
@@ -0,0 +1,102 @@
+package com.horner.polynome;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.horner.polynome.PolynomialApplication.Coefficients;
+
+public class HornerPolynomial {
+	
+	private List<Integer> rangeCoefficientsList;
+	private List<Double> randomCoefficientsList;
+	private Map<Double, Double>	mapResults = new HashMap<>();
+	private Number[] numberCoefficientsArray;
+	
+	
+	protected HornerPolynomial (final List<Integer> rangeCoefficientsList, 
+			final List<Double> randomCoefficientsList) {
+		this.rangeCoefficientsList = rangeCoefficientsList;
+		this.randomCoefficientsList = randomCoefficientsList;
+	}
+	
+	protected void setCoefficientsList (final List<? extends Number> coefficientsList){
+		Number[] numberCoefficientsArray = new Number[coefficientsList.size()];
+		this.numberCoefficientsArray = coefficientsList.toArray(numberCoefficientsArray);
+	}
+	
+	protected void computePolynomialValue (double value){
+		// Convert Array to a List
+		List<Number> coefficients = Arrays.stream(this.numberCoefficientsArray).collect(Collectors.toList());
+		
+		// Reverse Coefficients Order List
+		coefficients = coefficients.stream()
+	                               .sorted(Collections.reverseOrder())
+	                               .collect(Collectors.toList());
+		// Retrieve first value
+		Double accumulator = coefficients.get(0).doubleValue();
+		
+		// Convert List to Array
+		this.numberCoefficientsArray = coefficients.stream().toArray(Number[]::new);
+	    
+		// Generate new list excluding first member 
+	    coefficients = IntStream
+	    	      .range(1, this.numberCoefficientsArray.length)
+	    	      .mapToObj(i -> this.numberCoefficientsArray[i])
+	    	      .collect(Collectors.toList());
+	    
+	    // Loop through coefficients list 
+	    for (Number coefficient : coefficients) {
+            accumulator = (accumulator * value) + coefficient.doubleValue();
+        }
+	    
+	    // Store results into a Map
+		mapResults.put(value, accumulator);
+	}
+	
+	protected void runProcess(final List<? extends Number> coefficientsList, final Coefficients coefficients) {
+		initializeResults(coefficients);
+		setCoefficientsList(coefficientsList);
+		computePolynomialValue(0);
+		computePolynomialValue(100);
+		computePolynomialValue(9999);
+		displayResults();
+	}
+	
+	protected void displayResults() {
+		mapResults.entrySet().stream().forEach(entry -> System.out.println("For polynomial function input : " + entry.getKey() + " , result value is : " + entry.getValue()));
+		
+	}
+	
+	protected void initializeResults(final Coefficients coefficients) {
+		
+		switch (coefficients) {
+	        case RANGE_COEFFICIENT:
+	            System.out.println("Running process for coefficients list with value range");
+	            break;
+	                
+	        case RANDOM_COEFFICIENT:
+	            System.out.println("Running process for coefficients list with random value");
+	            break;
+	                     
+	       default:
+	            break;
+       }
+	   mapResults.clear();
+	}
+	
+	protected List<Integer> getRangeCoefficientsList() {
+		return rangeCoefficientsList;
+	}
+
+	protected List<Double> getRandomCoefficientsList() {
+		return randomCoefficientsList;
+	}
+
+	   
+}
---
+++ src/com/horner/polynome/PolynomialApplication.java
@@ -0,0 +1,54 @@
+package com.horner.polynome;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class PolynomialApplication {
+	public enum Coefficients {
+	    RANGE_COEFFICIENT, 
+	    RANDOM_COEFFICIENT 
+	}
+	
+	private HornerPolynomial polynomial;
+	private static final long DEGRE = 10000;
+		
+	// Constructor
+	public PolynomialApplication (HornerPolynomial polynomial) {
+			this.polynomial = polynomial;
+	}
+	
+	/*
+	 * 
+	 * 1 - Générer une liste de coefficients allant de 1 à 10000 et calculer P(0), P(100) et P(9999).
+	 * 2 - Générer une liste de 10000 coefficients prenant des valeurs aléatoires entre 0 et 1. Calculer P(0), P(100) et P(9999).
+	 *
+	 */
+	public static void main(String[] args) {
+		System.out.println("Starting process for polynomial application");
+		
+		PolynomialApplication app =  new PolynomialApplication (
+				new HornerPolynomial(
+						Stream.iterate(1, n -> n + 1)
+							  .limit(DEGRE)
+                              .collect(Collectors.toList()),
+				        new Random().doubles (DEGRE, 0, 1)
+				        .boxed().collect(Collectors.toList())
+				    )
+				); 
+		final List<Integer> rangeCoefficientsList = app.getPolynomial().getRangeCoefficientsList();
+		final List<Double> randomCoefficientsList = app.getPolynomial().getRandomCoefficientsList();
+		app.getPolynomial().runProcess(rangeCoefficientsList, Coefficients.RANGE_COEFFICIENT);
+		app.getPolynomial().runProcess(randomCoefficientsList, Coefficients.RANDOM_COEFFICIENT);
+		
+		System.out.println("Ending process for polynomial application");
+	}
+	
+	
+
+	private HornerPolynomial getPolynomial() {
+		return polynomial;
+	}
+
+}
